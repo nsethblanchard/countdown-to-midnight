baa98ee2e414e4afeca2b2ca84d9bfb538834dfb
Seth Blanchard <nsethblanchard@gmail.com>
2020-10-03 08:46:46 -0400

    done

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,12 @@
-#write your code here
 
 def countdown(int)
     while int > 0
         puts "#{int} SECOND(S)!"
         int -= 1  
-    end
-    
-    def countdown_with_sleep(seconds)
-        sleep 5
-    end
-    
+    end     
     return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+    sleep seconds
 end
